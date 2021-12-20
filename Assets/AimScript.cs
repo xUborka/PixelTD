661a144a9d03d24878c05753e1e5e5f5589e5a8a
xUborka <xgurke@gmail.com>
2021-12-20 16:57:27 +0100

    Partially fixed rotate

--- Assets/AimScript.cs
+++ Assets/AimScript.cs
@@ -16,9 +16,18 @@ public class AimScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = target.transform.position - transform.position;
-        Quaternion look_rotation = Quaternion.LookRotation(dir);
-        Vector3 rotation_target = Quaternion.Lerp(turret.transform.rotation, look_rotation, Time.deltaTime * 150f).eulerAngles;
-        turret.transform.rotation = Quaternion.Euler(0f, 0f, rotation_target.z);
+        Vector3 targ = target.transform.position;
+        Vector3 objectPos = turret.transform.position;
+        targ.x = targ.x - objectPos.x;
+        targ.y = targ.y - objectPos.y;
+
+        float angle = Mathf.Atan2(targ.y, targ.x) * Mathf.Rad2Deg;
+        turret.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        // Vector3 dir = target.transform.position - transform.position;
+        // Quaternion look_rotation = Quaternion.LookRotation(dir);
+        // Vector3 rotation_target = Quaternion.Lerp(turret.transform.rotation, look_rotation, Time.deltaTime * 10f).eulerAngles;
+        // print(rotation_target);
+        // turret.transform.rotation = Quaternion.Euler(0f, 0f, rotation_target.z);
     }
 }
